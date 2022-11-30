9106a3f443d5bf7439e4afb974169db939ea3ea8
uzinnnnnnn <1126dbwls@kookmin.ac.kr>
2022-11-30 19:23:14 +0900

    카테고리 기능 추가

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "포스팅 테스트",
+        "excerpt":" ","categories": ["Github Blog"],
+        "tags": [],
+        "url": "/github%20blog/%ED%8F%AC%EC%8A%A4%ED%8C%85%ED%85%8C%EC%8A%A4%ED%8A%B8/",
+        "teaser": null
+      }]
