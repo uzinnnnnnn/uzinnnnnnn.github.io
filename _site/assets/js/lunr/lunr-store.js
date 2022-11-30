cf1e24daaca8a79735a0694b278a5182e165f508
uzinnnnnnn <1126dbwls@kookmin.ac.kr>
2022-11-30 21:16:08 +0900

    Google Analytics 추가

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
-        "title": "포스팅 테스트",
-        "excerpt":" ","categories": ["Github Blog"],
+        "title": "Google Analytics로 방문자 통계하기",
+        "excerpt":"사람들이 내 블로그에 얼마나 방문했을까? Google Analytics를 적용하여 알아보자 ✨ Google Analytics?✨ 구글에서 제공하는 웹로그 분석 도구 무료이다! Google Analytics 시작하기 이 글은 minimal mistakes 테마 기준으로 작성하였습니다 Google Analytice 에 접속하여 회원가입 또는 로그인을 해준다. 계정 설정 페이지가 나오면 ‘새 계정 이름’을 넣고 다음을 누른다. 속성 이름 -&gt; github...","categories": ["Github Blog"],
         "tags": [],
         "url": "/github%20blog/%ED%8F%AC%EC%8A%A4%ED%8C%85%ED%85%8C%EC%8A%A4%ED%8A%B8/",
         "teaser": null
